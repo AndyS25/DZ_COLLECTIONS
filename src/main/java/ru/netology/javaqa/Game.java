6f0c8a62cafa7319fcd17d917c43825b060702fe
Andrey Senkovich <senkovich@gmail.com>
2026-03-01 00:07:29 +0300

    Replacing arraylist with hashmap

--- src/main/java/ru/netology/javaqa/Game.java
+++ src/main/java/ru/netology/javaqa/Game.java
@@ -1,31 +1,39 @@
 package ru.netology.javaqa;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Game {
-    ArrayList<Player> players = new ArrayList<>();
+    //ArrayList<Player> players = new ArrayList<>();
+    HashMap<String, Player> players = new HashMap<>();
 
     public void register(Player player) {
 
-        players.add(player);
+        //players.add(player);
+        players.put(player.getName(), player);
     }
 
     public int round(String playerName1, String playerName2) {
 
-        if (findByName(playerName1) == null) {
+        //if (findByName(playerName1) == null) {
+        if (findByName(playerName1) == false) {
             throw new NotRegisteredException(
                     "Игрок " + playerName1 + " не зарегистрирован!"
             );
         }
 
-        if (findByName(playerName2) == null) {
+        //if (findByName(playerName2) == null) {
+        if (findByName(playerName2) == false) {
             throw new NotRegisteredException(
                     "Игрок " + playerName2 + " не зарегистрирован!"
             );
         }
 
-        Player player1 = findByName(playerName1);
-        Player player2 = findByName(playerName2);
+        //Player player1 = findByName(playerName1);
+        //Player player2 = findByName(playerName2);
+
+        Player player1 = players.get(playerName1);
+        Player player2 = players.get(playerName2);
 
         if (player1.getStrength() > player2.getStrength()) {
             return 1;
@@ -36,13 +44,19 @@ public class Game {
         }
     }
 
-    public Player findByName(String name) {
+    /*public Player findByName(String name) {
         for (Player player : players) {
             if (player.getName().equals(name)) {
                 return player;
             }
         }
         return null;
+        return players.containsKey(name);
+    }*/
+
+    public boolean findByName(String name) {
+
+        return players.containsKey(name);
     }
 
 }
